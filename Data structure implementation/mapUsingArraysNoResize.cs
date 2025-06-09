50121b7630cb3021dbe6ac6cf13ce02baf175c00
lvanish <lvanishkoul@gmail.com>
2025-06-10 00:04:30 +0530

    add hash function uses chaining

--- Data structure implementation/mapUsingArraysNoResize.cs
+++ Data structure implementation/mapUsingArraysNoResize.cs
@@ -1,62 +1,135 @@
 using System;
-// coded on Visual studio
+
 MyMap map = new MyMap();
 
 map.insert(1, "one");
 map.insert(2, "two");
 map.insert(3, "three");
 
-//map.print();
+map.print();
 
-//map.delete(1);
-//map.print();
-if (map.containsKey(3))
-{
-    Console.WriteLine("present");
-}
+map.delete(1);
+map.print();
 
-class MyMap
+public class MyMap
 {
-    private string[] values = new string[100];
-    private bool[] vis = new bool[100];
+
+    private class Node
+    {
+        public int key;
+        public string value;
+        public Node next;
+        public Node(int key, string value)
+        {
+            this.key = key;
+            this.value = value;
+            next = null;
+        }
+    }
+    int bucketsize = 10;
+    private Node[] Buckets;
+
+    public MyMap()
+    {
+        Buckets = new Node[bucketsize];
+    }
+
+    public int hashindex(int key)
+    {
+        return Math.Abs(key) % bucketsize;
+    }
+
     public void insert(int key, string value)
     {
-        if(key < 0 || key >= 100)
+        int idx = hashindex(key);
+        Node head = Buckets[idx];
+
+        Node tmp = head;
+        while (tmp != null)
         {
-            Console.WriteLine("out of bounds");
-            return;
+            if (tmp.key == key)
+            {
+                tmp.value = value;
+                return;
+            }
+            tmp = tmp.next;
         }
-        values[key] = value;
-        vis[key] = true;
+
+        Node node = new Node(key, value);
+        node.next = head;
+        Buckets[idx] = node;
     }
 
+    ///---------------------------------------------------------------------------------
     public void delete(int key)
     {
-        if (key < 0 || key >= 100) { Console.WriteLine("out of bounds"); return; }
-        values[key] = null;
-        vis[key] = false;
-    }
-    public bool containsKey(int key) {
-        return values[key] != null;
-    }
-    public bool containsValue(string val)
-    {
-        for (int i = 0; i < 100; i++)
+        int idx = hashindex(key);
+        Node head = Buckets[idx];
+        Node prev = null;
+        Node curr = head;
+
+        while (curr != null)
         {
-            if(values[i] == val)
+            if (curr.key == key)
+            {
+                if (prev == null)
+                {
+                    Buckets[idx] = curr.next;
+                }
+                else
+                {
+                    prev.next = curr.next;
+                }
+                return;
+            }
+            prev = curr;
+            curr = curr.next;
+        }
+    }
+
+    public bool containsKey(int key)
+    {
+        int idx = hashindex(key);
+        Node head = Buckets[idx];
+        //Node tmp = head;
+        while (head != null)
+        {
+            if (head.key == key)
             {
                 return true;
             }
+            head = head.next;
         }
         return false;
     }
+
+    public bool containsValue(string val)
+    {
+        foreach (Node head in Buckets)
+        {
+            Node node = head;
+            while (node != null)
+            {
+                if (node.value == val)
+                {
+                    return true;
+                }
+                node = node.next;
+            }
+
+        }
+        return false;
+    }
+
     public void print()
     {
-        for(int i = 0;i < 100; i++)
+        for (int i = 0; i < bucketsize; i++)
         {
-            if (vis[i]  == true)
+            Node node = Buckets[i];
+            while (node != null)
             {
-                Console.WriteLine(i + " " + values[i]);
+                Console.WriteLine(node.key + " " + node.value);
+                node = node.next;
             }
         }
     }
